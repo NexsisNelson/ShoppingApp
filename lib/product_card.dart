280b75a8988aa7c0d61a0eb0a39be2d66048f765
Nexsis Nelson <nexsisnelson@gmail.com>
2026-02-11 14:58:19 +0100

    Create product_card.dart

---
+++ lib/product_card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  final String title;
+  final double price;
+  final String image;
+  final Color backgroundColor;
+
+  const ProductCard({
+    super.key,
+    required this.title,
+    required this.price,
+    required this.image,
+    required this.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+
+        color: backgroundColor,
+      ),
+      margin: EdgeInsets.all(10.0),
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(title, style: Theme.of(context).textTheme.titleMedium),
+
+          SizedBox(height: 5),
+
+          Text('\$ $price', style: Theme.of(context).textTheme.bodySmall),
+
+          SizedBox(height: 5),
+
+          Center(child: Image(image: AssetImage(image), height: 175)),
+        ],
+      ),
+    );
+  }
+}
